1e401fe5e39af1023cdb53e7422e1e9bd0231ac4
shagocart <shagocart@gmail.com>
2026-04-03 23:53:34 +0800

    Allow mobile runtime telemetry events

--- geezle-backend/src/controllers/apps.controller.ts
+++ geezle-backend/src/controllers/apps.controller.ts
@@ -21,7 +21,17 @@ const KNOWN_EVENTS = new Set([
   'prompt_suppressed',
   'campaign_opened',
   'push_registration_error',
-  'push_token_registered'
+  'push_token_registered',
+  'mobile_runtime_error',
+  'chunk_load_recovery',
+  'route_sync_recovery',
+  'deep_link_opened',
+  'deep_link_invalid',
+  'push_permission_denied',
+  'push_notification_received',
+  'push_notification_opened',
+  'socket_connect_error',
+  'socket_reconnected'
 ]);
 
 type CampaignRecord = {
